b77062c396314d2fcd1175cd5d0b11c403c5939a
ForeverZer0 <Zvezda2013!Svitlana>
2020-09-06 06:33:04 -0400

    Implemented comment deletion

--- Ruqqus.NET/RuqqusClient.Delete.cs
+++ Ruqqus.NET/RuqqusClient.Delete.cs
@@ -1,7 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
 namespace Ruqqus.NET
 {
     public partial class RuqqusClient
     {
-        
+        /// <summary>
+        /// Deletes a previously created comment.
+        /// </summary>
+        /// <param name="comment">A <see cref="Comment"/> instance that was created by this user.</param>
+        /// <returns><c>true</c> if operation completed successfully, otherwise <c>false</c>.</returns>
+        [Authorization(AuthorityKind.Required, OAuthScope.Delete)]
+        public async Task<bool> DeleteComment([NotNull] Comment comment)
+        {
+            if (comment is null)
+                throw new ArgumentNullException(nameof(comment));
+            return await DeleteComment(comment.Id);
+        }
+
+        /// <summary>
+        /// Deletes a previously created comment.
+        /// </summary>
+        /// <param name="commentId">The ID of a <see cref="Comment"/> that was created by this user.</param>
+        /// <returns><c>true</c> if operation completed successfully, otherwise <c>false</c>.</returns>
+        [Authorization(AuthorityKind.Required, OAuthScope.Delete)]
+        public async Task<bool> DeleteComment([NotNull] string commentId)
+        {
+            if (string.IsNullOrEmpty(commentId))
+                throw new ArgumentNullException(nameof(commentId));
+            if (!ValidSubmission.IsMatch(commentId))
+                throw new FormatException($"Invalid comment ID \"{commentId}\".");
+
+            await AssertAuthorizationAsync();
+            var uri = new Uri($"/api/v1/delete/comment/{commentId}", UriKind.Relative);
+            var response = await httpClient.PostAsync(uri, null);
+            return response.IsSuccessStatusCode;
+        }
     }
 }
